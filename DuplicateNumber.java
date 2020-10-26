cbd475f6ae8fcdc48f00180e1d804d1ba91484ca
ahujasherry <ahujasherry18@gmail.com>
2020-10-26 12:35:46 +0530

    Duplicate Number in array : adding freq method

--- DuplicateNumber.java
+++ DuplicateNumber.java
@@ -3,6 +3,7 @@
  * 
  * Given an array of n elements which contains elements from 0 to n-1, 
  * with any of these numbers appearing any number of times. Find these repeating numbers in O(n) 
+ * there is atleast one duplicate present
  * 
  */
 
@@ -47,20 +48,45 @@ public class DuplicateNumber
 	}
 	
 	
-
+	//2. Frequency method, also works for multiple repeated elements
+	public void DupFreq()
+	{
+		int size=list.size();
+		int index,freq;
+		
+		//increase the frequency or value of elements by n
+		for(int i=0;i<size;i++)
+		{
+			index= (list.get(i)) % size;
+			list.set(index, (list.get(index)  + size) ) ;
+		}
+		
+		//repeating elements are with freq>1
+		for(int i=0;i<size;i++)
+		{
+			freq= list.get(i) / size;
+			if(freq >1) 
+			{ 
+					System.out.println("elem : "+ i); 
+			}
+		}
+	
+			
+		return ;
+	}
 	
 
 	public static void main(String[] args) 
 	{
 	
 			DuplicateNumber obj=new DuplicateNumber();
-			int arr[] = {1, 2, 3, 5,3,5}; 
+			int arr[] = {1, 2, 2, 4}; 
 			for (int i : arr) 
 			{
 				obj.list.add(i);
 			}
 			
-			System.out.print(obj.bitMan());
+			obj.DupFreq();
 	}
 
 }
