e800cf9d534dee87d0a0f4648f6a0d7c6d9000be
ahujasherry <ahujasherry18@gmail.com>
2020-10-28 00:02:39 +0530

    Longest Consecutive sequence in array: update O(n) best time complexity solution

--- LongestConsSequence.java
+++ LongestConsSequence.java
@@ -1,4 +1,10 @@
 import java.util.*;
+
+/*
+ * 
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * 
+ */
 public class LongestConsSequence
 {
 	    int ans[];
@@ -19,6 +25,8 @@ public class LongestConsSequence
 	    	System.out.println("");
 	    	
 	    }
+	    
+	    //time complexity= O(nlogn)+ O(n)
 	    public int longestConsecutive(int[] arr) 
 	    {
 	    	 if(arr.length==0) return 0;
@@ -50,12 +58,43 @@ public class LongestConsSequence
 	        
 	        
 	    }
+	    
+	    //best time complexity= O(n)
+	    public static int findLongestConseqSubseq(int arr[])
+	    {
+	    	int n=arr.length;
+	        HashSet<Integer> S = new HashSet<Integer>();
+	        int ans = 0;
+	 
+	        //adding all elements to hash set
+	        for (int i = 0; i < n; ++i)
+	            S.add(arr[i]);
+	 
+	        // check each possible sequence from the start
+	        // then update optimal length
+	        for (int i = 0; i < n; ++i) {
+	           
+	        	// if current element is the starting point of subseq
+	            if (!S.contains(arr[i] - 1)) {
+	                // Then check for next elements
+	                // in the sequence
+	                int j = arr[i];
+	                while (S.contains(j))
+	                    j++;
+	 
+	                // global ans update
+	                if (ans < j - arr[i])
+	                    ans = j - arr[i];
+	            }
+	        }
+	        return ans;
+	    }
 	
 	public static void main(String[] args) 
 	{
 		LongestConsSequence obj=new LongestConsSequence();
 		int arr[]=new int[] {0,1,1,2,3};
-		System.out.println(obj.longestConsecutive(arr));
+		System.out.println(obj.findLongestConseqSubseq(arr));
 
 	}
 
