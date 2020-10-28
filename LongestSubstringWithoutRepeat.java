5f540b43ac6b0d0338edde22fd71d97ff96836d4
ahujasherry <ahujasherry18@gmail.com>
2020-10-28 18:34:10 +0530

    Longest substring without repeating characters: 0(n) solution

--- LongestSubstringWithoutRepeat.java
+++ LongestSubstringWithoutRepeat.java
@@ -1,6 +1,6 @@
 /*
  * 
- * Given a string s, find the length of the longest substring without repeating characters.
+- * Given a string s, find the length of the longest substring without repeating characters.
  * 
  * 
  */
@@ -9,7 +9,46 @@ import java.util.*;
 public class LongestSubstringWithoutRepeat {
 
 	
-	//average time complexity -> O(n)
+	//Time complexity= O(N)
+	public static int subStrLinear(String str)
+	{
+		if(str.isEmpty()) return 0;
+		 if(str.length()==1) return 1;
+		int len=0,start=0,end=0,res=0;
+		
+		//array to store visited and last index
+		int visited[]=new int[256];
+		for (int k = 0; k < visited.length; k++) {
+			visited[k]=-1;
+		}
+		
+		while(end!=str.length())
+		{
+			//if in set
+			
+			if(visited[str.charAt(end)]!=-1)
+			{
+				visited[str.charAt(start)]=-1;
+				start++;
+			}
+			else
+			{
+				visited[str.charAt(end)]=0;
+				len=end-start+1;
+				end++;
+				
+			}
+			
+			res=Math.max(len, res);
+	
+			
+		}
+		
+			
+		return res;
+	}
+	
+	//average time complexity -> O(n2)
 	public static int subStr(String str)
 	{
 		if(str.isEmpty()) return 0;
@@ -19,6 +58,7 @@ public class LongestSubstringWithoutRepeat {
 		for (int i = 0; i < str.length(); i++) 
 		{
 			boolean visited[]=new boolean[256];
+			
 			for (int j = i; j < str.length(); j++) 
 			{
 				//if current jth character is already visited, break loop and move to next ith 
@@ -43,9 +83,9 @@ public class LongestSubstringWithoutRepeat {
 	}
 	public static void main(String[] args) 
 	{
-		String str = "pwwkew"; 
+		String str = "abcabcbb"; 
 	   
-	    int len = subStr(str); 
+	    int len = subStrLinear(str); 
 	    System.out.println("len: " + len); 
 		
 
