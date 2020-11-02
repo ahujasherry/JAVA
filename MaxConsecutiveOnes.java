0b775c9f669260ca9753a2a55e05ec797530cf36
ahujasherry <ahujasherry18@gmail.com>
2020-11-02 14:12:37 +0530

    update :find max consecutives ones in binary array: T.C -> O(n) , S.C -> O(1) smart solution

--- MaxConsecutiveOnes.java
+++ MaxConsecutiveOnes.java
@@ -7,6 +7,29 @@ import java.util.*;
 
 public class MaxConsecutiveOnes {
 
+	//smart solution
+	public static int findMaxConsecutiveOnes1(int[] arr) 
+    {
+        if(arr.length ==0 ) return 0;
+        if(arr.length ==1 && arr[0]==1) return 1;
+        
+        int sum=0;
+        int res=0;
+        
+        for(int ele:arr)
+        {
+        	//sum=1 show prev ele was 1, its consecutive
+            sum*=ele;
+            
+            //act as count
+            sum+=ele;
+            res=Math.max(sum,res);
+            
+        }
+        
+        return res;
+        
+    }
 	public static int findMaxConsecutiveOnes(int[] arr) 
     {
         if(arr.length ==0 ) return 0;
@@ -33,11 +56,11 @@ public class MaxConsecutiveOnes {
     }
 	public static void main(String[] args) 
 	{
-		 int arr[] = {1,0,1,1,0,1}; 
+		 int arr[] = {1,0,1,1,0,1,1,1}; 
                
 		 int n = arr.length; 
   
-		 System.out.println(findMaxConsecutiveOnes(arr)); 
+		 System.out.println(findMaxConsecutiveOnes1(arr)); 
 
 	}
 
