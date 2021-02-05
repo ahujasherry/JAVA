c90e9bb8687c966b51cc19ed2b86be968deb23c1
Sherry Ahuja <ahujasherry18@gmail.com>
2021-02-05 16:41:30 +0530

    Floor and Ceil BST

---
+++ FloorCeilBST.java
@@ -0,0 +1,60 @@
+
+
+public class FloorCeilBST
+{
+	
+	public static int Ceil(Node root, int key)
+	{
+		if(root==null) return -1;
+
+		if(root.val== key) return root.val;
+
+
+		//find in right subtree
+		if(root.val < key)
+		{
+			 return Ceil(root.right, key);
+		} 
+
+		//find in left subtree if present
+		else if( root.val > key)
+		{
+			int ans=Ceil(root.left, key);
+			return (ans>= key && ans!=-1) ? ans : root.val)
+
+		}
+
+
+	}
+
+
+	public static int floor(Node root, int key)
+	{
+		if(root==null) return -1;
+
+		if(root.val== key) return root.val;
+
+
+		//find in right subtree and find smallest of right subtree and check with key
+		if(root.val < key)
+		{
+			int ans=floor(root.right, key);
+			return (ans<= key && ans!=-1) ? ans : root.val)
+			 return Ceil(root.right, key);
+		} 
+
+		//find in left subtree if present
+		else if( root.val > key)
+		{
+			return floor(root.left, key);
+
+		}
+
+
+	}
+
+
+
+
+
+}
