d42cd0c613d52436e23e53197b9f2a69be569a97
ahujasherry <ahujasherry18@gmail.com>
2021-04-14 16:22:03 +0530

    update

--- MajorityElement.java
+++ MajorityElement.java
@@ -11,15 +11,24 @@ public int majorityElement1(int[] nums) {
 
 // Moore voting algorithm
 //n/2 times
-public int majorityElement3(int[] nums) {
-    int count=0, ret = 0;
-    for (int num: nums) {
-        if (count==0)
-            ret = num;
-        if (num!=ret)
-            count--;
-        else
+public int majorityElement3(int[] arr) 
+{
+
+    int count=1, major = arr[0];
+    for (int i=0;i<arr.length: i++) 
+    {
+        if (arr[i]==major)
             count++;
+        else
+        {
+            count--;
+            if(count==0)
+            {
+                major=arr[i];
+                count=1;
+            }
+        }
+        
     }
     return ret;
 }
