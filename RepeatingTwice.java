d8a05b2ce15bff9ecde34ec0f4a5adb949a05bde
ahujasherry <ahujasherry18@gmail.com>
2021-04-14 16:23:07 +0530

    update

--- RepeatingTwice.java
+++ RepeatingTwice.java
@@ -1,6 +1,6 @@
 /*
  * 
- * Find the two repeating elements in a given array
+ * 
  * You are given an array of n+2 elements. All elements of the array are in range 1 to n. 
  * And all elements occur once except two numbers which occur twice. Find the two repeating numbers.
  * 
