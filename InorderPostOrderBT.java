bf8bec88a70d986bab9a713672df42d86fdb789b
ahujasherry <ahujasherry18@gmail.com>
2021-04-14 16:21:13 +0530

    update

--- InorderPostOrderBT.java
+++ InorderPostOrderBT.java
@@ -1,60 +1,71 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) 
+class block {
+    int data;
+    block left, right;
+ 
+    public block(int data)
     {
-        
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
-        
-        //put inorder indeces in hashmap
-        for(int i=0;i<inorder.length;i++)
-        {
-            map.put(inorder[i],i);
-        }
-        
-        int len=inorder.length;
-        
-        
-        TreeNode root=buildHelper(postorder.length-1,postorder, inorder, 0 ,len-1,map);
-        return root;
-        
+        this.data = data;
+        left = right = null;
     }
-    public static TreeNode buildHelper(int postStart,int[] postorder, int[] inorder, int                    start, int end,HashMap<Integer,Integer> map)
+}
+ 
+class InorderPostorderBT {
+   static int postEnd;
+    public InorderPostorderBT(int n)
     {
-        //if postStart of postorder goes out of bound after jump or inorder goes out of bound
-        if((start > end) ||( postStart > postorder.length-1) ) return null;
+        postEnd=n-1;
+    }
+    public block buildUtil(int in[], int post[], int inStrt,
+                   int inEnd)
+    {
+        if (inStrt > inEnd)
+            return null;
+ 
+        block node = new block(post[postEnd--]);
+ 
+        if (inStrt == inEnd)
+            return node;
         
-        //make postStart array element as root
-        TreeNode node= new TreeNode(postorder[postStart]);
-        
-        //if leaf node
-        if(start == end ) return node;
-        
-        //get index of inorder of node from map
-        int inIndex=map.get(node.val);
-        
-        //left subtree will be inorder index left and right will be its right
-        //postStart jump will be like postStart-1-end+inIndex
-        node.left=buildHelper( postStart-1-end+inIndex,postorder,inorder,start,inIndex-1,map);
-       
-        //right subtree will be inorder index right
-        //postStart index of right subtree is postStart index -1
-        node.right=buildHelper(postStart-1,postorder,inorder,inIndex+1,end,map);
+        int iIndex = search(in, inStrt, inEnd, node.data);
+ 
+         node.right= buildUtil(in, post, iIndex + 1, inEnd);
+
+        node.left = buildUtil(in, post, inStrt, iIndex - 1);
+           
         
         return node;
+    }
+ 
+    
+    int search(int arr[], int strt, int end, int value)
+    {
+        int i;
+        for (i = strt; i <= end; i++) {
+            if (arr[i] == value)
+                break;
+        }
+        return i;
+    }
+ 
+    void preOrder(block node)
+    {
+        if (node == null)
+            return;
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int in[] = new int[] { 4, 8, 2, 5, 1, 6, 3, 7 };
+        int post[] = new int[] { 8, 4, 5, 2, 6, 7, 3, 1 };
+        int n = in.length;
+        InorderPostorderBT tree = new InorderPostorderBT(n);
         
+        block root= tree.buildUtil(in, post, 0, n - 1);
+        System.out.println(
+            "Preorder of the constructed tree : ");
+        tree.preOrder(root);
     }
 }
